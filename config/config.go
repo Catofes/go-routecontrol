954a9a7ca084ddf6f93e2fda9f79980b7a022769
Catofes <i@catofes.com>
2017-06-18 14:14:58 +0800

    Init.

---
+++ config/config.go
@@ -0,0 +1,31 @@
+/*
+Package config try to load config from a file and construct a Singleton Config Object.
+*/
+package config
+
+import (
+	"sync"
+)
+
+var mainConfig *JSONConfig
+var once *sync.Once
+
+// JSONConfig struct save all the config parameters.
+type JSONConfig struct {
+	PgPath string
+	Token  string
+}
+
+func (s *JSONConfig) init(path string) *JSONConfig {
+	return s
+}
+
+// GetInstance return the singleton instance.
+func GetInstance(path string) *JSONConfig {
+	if path != "" {
+		once.Do(func() {
+			mainConfig = (&JSONConfig{}).init(path)
+		})
+	}
+	return mainConfig
+}
